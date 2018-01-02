89b6df2503489e1f72ff17adc6d1cd39fb8addcc
abc235896 <bc140401486@vu.edu.pk>
2018-01-02 12:15:15 +0500

    correct enqueue style function name

--- guten-scripts.php
+++ guten-scripts.php
@@ -9,7 +9,7 @@ function guten_scripts_editor_asset() {
 	//enqueuing our blocks main script
 	wp_enqueue_script( 'guten-script', plugins_url( 'index.js', __FILE__ ), array( 'wp-blocks', 'wp-element' ) , filemtime( plugin_dir_path( __FILE__ ) . 'index.js' ));
 	//enqueuing our blocks editor style sheet
-	wp_enqueue_stype ( 'guten-script-editor-style', plugin_url( 'editor.css', __FILE__ ), array(), filemtime( plugin_dir_path( __FILE__ ) . 'editor.css' ) );
+	wp_enqueue_style ( 'guten-script-editor-style', plugin_url( 'editor.css', __FILE__ ), array(), filemtime( plugin_dir_path( __FILE__ ) . 'editor.css' ) );
 
 }
 
